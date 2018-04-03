c40b09a82bd6791772256711f7cf97b7060bc89e
Andrew Heppner <andrewheppner@Andrews-MacBook-Pro-2.local>
2018-04-03 10:43:25 -0400

    Remove unused imports

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import { AppRegistry } from "react-native";
 import App from "./App"; // Newest
-import CircleSlider from "./rayChoi";
-import CircleSlider2 from "./CircularSliderApp";
 AppRegistry.registerComponent("circle_slider", () => App);
